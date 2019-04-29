f4ebe212b1f169bd4e24d593a2f60b74ab6ab273
Son Chau <soncqb@gmail.com>
2019-04-29 13:02:20 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:bloc_pattern/src/app.dart';
+
+void main() => runApp(App());
+
---
+++ lib/src/models/item_model.dart
@@ -0,0 +1,6 @@
+class ItemModel{
+  int _page;
+  int _total_results;
+  int _total_pages;
+  //List <_Result> _results = []
+}
